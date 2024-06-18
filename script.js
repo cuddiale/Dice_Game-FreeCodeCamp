cee177ea4c75486c09d68ba967f79bb3481f8536
cuddiale <alessiocuddretto@hotmail.com>
2024-06-18 12:30:02 +0200

    Update

---
+++ script.js
@@ -0,0 +1,7 @@
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const currentRoundText = document.getElementById("current-round");
+const currentRoundRollsText = document.getElementById("current-round-rolls");
+const totalScoreText = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
