aa1e39f0bd5d584c439261e4b7f5da302072ccf5
cuddiale <alessiocuddretto@hotmail.com>
2024-06-19 09:34:00 +0200

    Update

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ const rulesBtn = document.getElementById("rules-btn");
 
 let diceValuesArr = [];
 let isModalShowing = false;
+let score = 0;
+let totalScore = 0;
+let round = 1;
+let rolls = 0;
+
+const rollDice = () => {};
+
+rulesBtn.addEventListener("click", () => {
+  isModalShowing = !isModalShowing;
+
+  if (isModalShowing) {
+    rulesBtn.textContent = "Hide Rules";
+    rulesContainer.style.display = "block";
+  } else {
+    rulesBtn.textContent = "Show Rules";
+    rulesContainer.style.display = "none";
+  }
+});
