b72bd9af1c61bdd2444e670f6722132099ba796e
cuddiale <alessiocuddretto@hotmail.com>
2024-06-19 09:32:32 +0200

    Update

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const currentRoundText = document.getElementById("current-round");
 const currentRoundRollsText = document.getElementById("current-round-rolls");
 const totalScoreText = document.getElementById("total-score");
 const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesContainer = document.querySelector(".rules-container");
+const rulesBtn = document.getElementById("rules-btn");
+
+let diceValuesArr = [];
+let isModalShowing = false;
